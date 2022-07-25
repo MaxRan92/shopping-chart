7442d016ddde9f758ca8ba62601c11a5ef2177e4
Massimo Ranalli <ranallimax@gmail.com>
2022-07-25 15:32:04 +0000

    migrate initial algos model

--- algos/admin.py
+++ algos/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Algo, Category
 
 # Register your models here.
+admin.site.register(Algo)
+admin.site.register(Category)
--- algos/models.py
+++ algos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_lenght=254)
+    name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
@@ -19,5 +19,7 @@ class Algo(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
