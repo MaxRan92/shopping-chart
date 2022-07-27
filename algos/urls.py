81c918ce1ce00b3ad85d9581930362628bb8d0b1
Massimo Ranalli <ranallimax@gmail.com>
2022-07-27 21:56:37 +0000

    insert name, price, rating and image in the algo_details page

--- algos/urls.py
+++ algos/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_algos, name='algos'),
-    path('algo_id', views.algo_detail, name='algo_detail'),
+    path('<algo_id>', views.algo_detail, name='algo_detail'),
 ]
