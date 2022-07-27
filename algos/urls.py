8b28c9868acbf9956159badce8c2dc15324e0f49
Massimo Ranalli <ranallimax@gmail.com>
2022-07-27 21:38:09 +0000

    Create new view, url and template for the algo_detail page

--- algos/urls.py
+++ algos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_algos, name='algos'),
+    path('algo_id', views.algo_detail, name='algo_detail'),
 ]
--- algos/views.py
+++ algos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Algo
 from shopping_chart.settings import MEDIA_URL
 
@@ -17,3 +17,18 @@ def all_algos(request):
     }
 
     return render(request, 'algos/algos.html', context)
+
+
+def algo_detail(request, algo_id):
+    """
+    A view to show the detailed page of one algorithm
+    """
+
+    algo = get_object_or_404(Algo, pk=algo_id)
+
+    context = {
+        'algo': algo,
+        'MEDIA_URL': MEDIA_URL,
+    }
+
+    return render(request, 'algos/algo_detail.html', context)
