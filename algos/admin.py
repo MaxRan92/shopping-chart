37250432bd666487c105b31cce0824e808f0e375
Massimo Ranalli <ranallimax@gmail.com>
2022-07-27 16:34:54 +0000

    Customized Admin, fix Categroys typo

--- algos/admin.py
+++ algos/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 from .models import Algo, Category
 
 # Register your models here.
-admin.site.register(Algo)
-admin.site.register(Category)
+class AlgoAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Algo, AlgoAdmin)
+admin.site.register(Category, CategoryAdmin)
