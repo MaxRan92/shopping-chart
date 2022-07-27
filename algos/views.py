ea3117b8d79cfc5e28ec573a8345ba034f88f4b4
Massimo Ranalli <ranallimax@gmail.com>
2022-07-27 16:45:18 +0000

    Create algos.html page to show specific product page, set up urls and views

--- algos/views.py
+++ algos/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Algo
 
 # Create your views here.
+
+def all_algos(request):
+    """
+    A view to show all products, including sorting and search queries
+    """
+
+    algos = Algo.objects.all()
+
+    context = {
+        'algos': algos,
+    }
+
+    return render(request, 'algos/algos.html', context)
