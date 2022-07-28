aaab069e39bedd850730a1074e885378648a7534
Massimo Ranalli <ranallimax@gmail.com>
2022-07-28 19:50:24 +0000

    Added category filtering

--- algos/views.py
+++ algos/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Algo
+from .models import Algo, Category
 from shopping_chart.settings import MEDIA_URL
 
 # Create your views here.
@@ -13,8 +13,14 @@ def all_algos(request):
 
     algos = Algo.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            algos = algos.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -28,6 +34,7 @@ def all_algos(request):
         'algos': algos,
         'MEDIA_URL': MEDIA_URL,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'algos/algos.html', context)
