4b7af69f38968beff35ffa73606f19e8755d23e0
Massimo Ranalli <ranallimax@gmail.com>
2022-07-28 21:58:08 +0000

    in algos.html add dropdown menu for sorting, search counter and home link

--- algos/views.py
+++ algos/views.py
@@ -25,6 +25,8 @@ def all_algos(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 algos = algos.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
