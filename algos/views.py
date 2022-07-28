8d1a4e4857773b75cddfab0547695b45eeec28eb
Massimo Ranalli <ranallimax@gmail.com>
2022-07-28 17:25:38 +0000

    Added search functionality to return algos that match word in their name or description

--- algos/views.py
+++ algos/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Algo
 from shopping_chart.settings import MEDIA_URL
 
@@ -10,10 +12,22 @@ def all_algos(request):
     """
 
     algos = Algo.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any serach criteria!")
+                return redirect(reverse('algos'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            algos = algos.filter(queries)
 
     context = {
         'algos': algos,
         'MEDIA_URL': MEDIA_URL,
+        'search_term': query,
     }
 
     return render(request, 'algos/algos.html', context)
