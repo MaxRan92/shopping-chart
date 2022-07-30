dc56886843f88cb060c9b9ebd061575e1a1da8c1
Massimo Ranalli <ranallimax@gmail.com>
2022-07-30 23:14:54 +0000

    Added toasts framework to include notification when a new product is updated or deleted

--- bag/views.py
+++ bag/views.py
@@ -17,13 +17,14 @@ def add_to_bag(request, item_id):
     Add a quantity of the specified product to the shopping bag
     """
 
-    algo = Algo.objects.get(pk=item_id)
+    algo = get_object_or_404(Algo, pk=item_id)
     license_period = int(request.POST.get('license_period'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list (bag.keys()):
         bag[item_id] += license_period
+        messages.success(request, f'Updated {algo.name} license period to {bag[item_id]} months')
     else:
         bag[item_id] = license_period
         messages.success(request, f'Added {algo.name} to your bag')
@@ -37,13 +38,16 @@ def adjust_bag(request, item_id):
     Adjust the quantity of the specified product to the specified license period
     """
 
+    algo = get_object_or_404(Algo, pk=item_id)
     license_period = int(request.POST.get('license_period'))
     bag = request.session.get('bag', {})
 
     if license_period > 0:
         bag[item_id] = license_period
+        messages.success(request, f'Updated {algo.name} license period to {bag[item_id]} months')
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed {algo.name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -55,10 +59,13 @@ def remove_from_bag(request, item_id):
     """
 
     try:
+        algo = get_object_or_404(Algo, pk=item_id)
         bag = request.session.get('bag', {})
         bag.pop(item_id)
+        messages.success(request, f'Removed {algo.name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
