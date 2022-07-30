ab116314e691f51d8ff31b7fc6158d7bc93f26bf
Massimo Ranalli <ranallimax@gmail.com>
2022-07-30 09:52:16 +0000

    Added add to bag functionality, adds time periods to every instrument

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -8,3 +8,21 @@ def view_bag(request):
     A view to renders the bag content page
     """
     return render(request, 'bag/bag.html')
+
+def add_to_bag(request, item_id):
+    """
+    Add a quantity of the specified product to the shopping bag
+    """
+
+    licensing_period = int(request.POST.get('licensing_period'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list (bag.keys()):
+        bag[item_id] += licensing_period
+    else:
+        bag[item_id] = licensing_period
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
