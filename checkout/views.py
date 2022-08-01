446debc923a185e44d0b288fd4afd184a6f3a680
Massimo Ranalli <ranallimax@gmail.com>
2022-08-01 23:19:54 +0000

    Added checkout form data caching in payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -8,6 +9,24 @@ from algos.models import Algo
 from bag.contexts import bag_contents
 
 import stripe
+import json
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
+
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
@@ -41,7 +60,7 @@ def checkout(request):
                     order_line_item.save()
                 except Algo.DoesNotExist:
                     messages.error(request, (
-                        "One of the algorithms in your bag wasn't found in our database. "
+                        "One of the products in your bag wasn't found in our database. "
                         "Please call us for assistance!")
                     )
                     order.delete()
@@ -56,7 +75,7 @@ def checkout(request):
         bag = request.session.get('bag', {})
         if not bag:
             messages.error(request, "There's nothing in your bag at the moment")
-            return redirect(reverse('algos'))
+            return redirect(reverse('products'))
 
         current_bag = bag_contents(request)
         total = current_bag['grand_total']
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,13 +2,11 @@ from django.http import HttpResponse
 
 
 class StripeWH_Handler:
-    """
-    Handle Stripe webhooks
-    """
+    """Handle Stripe webhooks"""
 
     def __init__(self, request):
         self.request = request
-    
+
     def handle_event(self, event):
         """
         Handle a generic/unknown/unexpected webhook event
@@ -16,16 +14,18 @@ class StripeWH_Handler:
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
             status=200)
-    
-    def handle_payment_event_succeeded(self, event):
+
+    def handle_payment_intent_succeeded(self, event):
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
+        intent = event.data.object
+        print(intent)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
-        
-    def handle_payment_event_payment_failed(self, event):
+
+    def handle_payment_intent_payment_failed(self, event):
         """
         Handle the payment_intent.payment_failed webhook from Stripe
         """
