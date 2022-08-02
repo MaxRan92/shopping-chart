e09d74ad08d08b6ddb222f100a47865985016e05
Massimo Ranalli <ranallimax@gmail.com>
2022-08-02 22:04:39 +0000

    Added profile form, views and template updated

---
+++ profiles/static/profiles/js/countryfield.js
@@ -0,0 +1,12 @@
+let countrySelected = $('#id_default_country').val();
+if(!countrySelected) {
+    $('#id_default_country').attr('style', 'color: #aab7c4 !important');
+};
+$('#id_default_country').change(function() {
+    countrySelected = $(this).val();
+    if(!countrySelected) {
+        $(this).attr('style', 'color: #aab7c4 !important');
+    } else {
+        $(this).attr('style', 'color: #ffffff');
+    }
+});
