2a68079c958b218bfd010c1691f47a406cfbc0f1
Massimo Ranalli <ranallimax@gmail.com>
2022-08-07 08:57:25 +0000

    separate footer in proper html file, create newsletter html to post email

--- home/views.py
+++ home/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from newsletter.forms import NewsletterForm
+from newsletter.models import Newsletter
+from django.contrib import messages
 
 # Create your views here.
 
+
 def index(request):
-    """
-    A view to return the index page
-    """
-    return render(request, 'home/index.html')
+    """ A view to return the index page """
+
+    form = NewsletterForm(request.POST or None)
+
+    if request.method == 'POST':
+        print(form.errors)
+        if form.is_valid:
+            form.save()
+            messages.success(request, 'Thank you for subscribing to our newsletter!')
+            return redirect(reverse('home'))
+        messages.error(request, 'There was an error please try again')
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'home/index.html', context)
