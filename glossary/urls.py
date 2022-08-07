28c591f9256631be5a915b49d54f2f10449fe142
Massimo Ranalli <ranallimax@gmail.com>
2022-08-07 11:18:47 +0000

    Glossary term delete function implemented

--- glossary/urls.py
+++ glossary/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.view_glossary, name='view_terms'),
+    path('<int:pk>/delete_comment/',
+         views.TermDelete.as_view(), name="delete_term"),
 ]
--- glossary/views.py
+++ glossary/views.py
@@ -2,16 +2,25 @@ from django.shortcuts import render, reverse, redirect
 from .models import Terms
 from .forms import TermForm
 from django.contrib import messages
+from django.views.generic import DeleteView, UpdateView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
 
 def view_glossary(request):
     """
-    A view to return the blog page
+    A view to return the glossary page
     """
+    
     terms = Terms.objects.all()
     form = TermForm(request.POST or None)
+
+    # if term_deleted:
+    #     messages.success(request, f'The term has been deleted!')
+    #     term_deleted = False
 
     if request.method == 'POST':
         form = TermForm(request.POST)
@@ -28,3 +37,20 @@ def view_glossary(request):
     }
     
     return render(request, 'glossary.html', context)
+
+
+@method_decorator(login_required, name="dispatch")
+class TermDelete(DeleteView):
+    """
+    If user is logged in:
+    If is superuser, delete the term
+    """
+    model = Terms
+    template_name = "delete_term.html"
+
+    def delete(self, request, *args, **kwargs):
+        return super(TermDelete, self).delete(request, *args, **kwargs)
+
+    def get_success_url(self, *args, **kwargs):
+        view_glossary.term_deleted = True
+        return reverse("view_terms")
