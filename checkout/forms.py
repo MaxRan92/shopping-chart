9ba2102d5402e0d1824cae3bfc2532663e699ce7
Massimo Ranalli <ranallimax@gmail.com>
2022-08-08 11:05:51 +0000

    revert to yesterday the checkout code - fix copypaste error

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,79 +1,39 @@
-import uuid
-from django.db import models
-from django.db.models import Sum
-from django.conf import settings
+from django import forms
+from .models import Order
 
-from django_countries.fields import CountryField
 
-from algos.models import Algo
-from profiles.models import UserProfile
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number',
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'postcode', 'country',
+                  'county',)
 
-# Create your models here.
-class Order(models.Model):
-
-    order_number = models.CharField(max_length=32, null=False, editable=False)
-    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
-                                     null=True, blank=True, related_name='orders')
-    full_name = models.CharField(max_length=50, null=False, blank=False)
-    email = models.EmailField(max_length=254, null=False, blank=False)
-    phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = CountryField(blank_label='Country *', null=False, blank=False)
-    postcode = models.CharField(max_length=20, null=True, blank=True)
-    town_or_city = models.CharField(max_length=40, null=False, blank=False)
-    street_address1 = models.CharField(max_length=80, null=False, blank=False)
-    street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    county = models.CharField(max_length=80, null=True, blank=True)
-    date = models.DateTimeField(auto_now_add=True)
-    discount = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
-    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    original_bag = models.TextField(null=False, blank=False, default='')
-    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
-
-    def _generate_order_number(self):
+    def __init__(self, *args, **kwargs):
         """
-        Generate a random and unique order number
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
         """
-        return uuid.uuid4().hex.upper()
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'postcode': 'Postal Code',
+            'town_or_city': 'Town or City',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'county': 'County, State or Locality',
+        }
 
-    def update_total(self):
-        """
-        Update grand total each time a line item is added,
-        accounting for delivery costs.
-        """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
-        if self.order_total > settings.DISCOUNT_THRESHOLD:
-            self.discount = self.order_total * settings.DISCOUNT_PERCENTAGE/100
-        else:
-            self.discount = 0
-        self.grand_total = self.order_total - self.discount
-        self.save()
-    
-    def save(self, *args, **kwargs):
-        """
-        Override the original save method to set the order
-        number if it hasn't been set already
-        """
-        if not self.order_number:
-            self.order_number = self._generate_order_number()
-        super().save(*args, *kwargs)
-    
-    def __str__(self):
-        return self.order_number
-
-class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name="lineitems")
-    algo = models.ForeignKey(Algo, null=False, blank=False, on_delete=models.CASCADE)
-    license_period = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
-
-    def save(self, *args, **kwargs):
-        """
-        Override the original save method to set the lineitem
-        total and update order total
-        """
-        self.lineitem_total = self.algo.price * self.license_period
-        super().save(*args, **kwargs)
-
-    def __str__(self):
-        return f'SKU {self.algo.sku} on order {self.order.order_number}'
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
