325463616d383cfd09b4e6f433f1dc0b319cc6ce
Massimo Ranalli <ranallimax@gmail.com>
2022-08-08 10:34:26 +0000

    Revert changes on checkout wh handler - 500 error solving

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,17 +1,14 @@
-"""
-Credits: the code is inspired and adapted from the
-Code Institute Boutique Ado Project
-"""
 from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
+
 from .models import Order, OrderLineItem
 from algos.models import Algo
 from profiles.models import UserProfile
+
 import json
 import time
-
 
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
@@ -28,13 +25,14 @@ class StripeWH_Handler:
         body = render_to_string(
             'checkout/confirmation_emails/confirmation_email_body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
-
+        
         send_mail(
             subject,
             body,
             settings.DEFAULT_FROM_EMAIL,
             [cust_email]
-        )
+        )        
+
 
     def handle_event(self, event):
         """
@@ -72,13 +70,12 @@ class StripeWH_Handler:
                 profile.default_country = shipping_details.address.country
                 profile.default_postcode = shipping_details.address.postal_code
                 profile.default_town_or_city = shipping_details.address.city
-                profile.default_street_address1 = shipping_details.address.line1  # noqa
-                profile.default_street_address2 = shipping_details.address.line2  # noqa
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
                 profile.default_county = shipping_details.address.state
                 profile.save()
 
-        # Checks if order exists, otherwise will create one with
-        # the billing and shipping details
+        # Checks if order exists, otherwise will create one with the billing and shipping details
         order_exists = False
         attempt = 1
         while attempt <= 5:
@@ -105,8 +102,7 @@ class StripeWH_Handler:
         if order_exists:
             self._send_confirmation_email(order)
             return HttpResponse(
-                content=f'Webhook received: {event["type"]} | SUCCESS: \
-                          Verified order already in database',
+                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                 status=200)
         else:
             order = None
@@ -141,8 +137,7 @@ class StripeWH_Handler:
                     status=500)
         self._send_confirmation_email(order)
         return HttpResponse(
-            content=f'Webhook received: {event["type"]} | SUCCESS: \
-                      Created order in webhook',
+            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
