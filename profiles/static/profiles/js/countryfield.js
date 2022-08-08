f8952f520b66ddd120d76f5520dba7f470a6a4ab
Massimo Ranalli <ranallimax@gmail.com>
2022-08-08 20:21:49 +0000

    PEP8 validation fixes

--- profiles/static/profiles/js/countryfield.js
+++ profiles/static/profiles/js/countryfield.js
@@ -1,7 +1,8 @@
+/* jshint esversion: 11, jquery: true */
 let countrySelected = $('#id_default_country').val();
 if(!countrySelected) {
     $('#id_default_country').attr('style', 'color: #aab7c4 !important');
-};
+}
 $('#id_default_country').change(function() {
     countrySelected = $(this).val();
     if(!countrySelected) {
